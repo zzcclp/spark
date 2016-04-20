fa67c9c530195ceeabcc1242a242f2ec0a3a1dd4
Zhang Zhichao <441586683@qq.com>
2016-04-20 10:33:29 +0800

    [EXT][SPARK-13904] Add exit code parameter to exitExecutor() #12457

--- core/src/main/scala/org/apache/spark/executor/CoarseGrainedExecutorBackend.scala
+++ core/src/main/scala/org/apache/spark/executor/CoarseGrainedExecutorBackend.scala
@@ -67,7 +67,7 @@ private[spark] class CoarseGrainedExecutorBackend(
         // Always receive `true`. Just ignore it
       case Failure(e) => {
         logError(s"Cannot register with driver: $driverUrl", e)
-        System.exit(1)
+        exitExecutor(1)
       }
     }(ThreadUtils.sameThread)
   }
@@ -84,12 +84,12 @@ private[spark] class CoarseGrainedExecutorBackend(
 
     case RegisterExecutorFailed(message) =>
       logError("Slave registration failed: " + message)
-      System.exit(1)
+      exitExecutor(1)
 
     case LaunchTask(data) =>
       if (executor == null) {
         logError("Received LaunchTask command but executor was null")
-        System.exit(1)
+        exitExecutor(1)
       } else {
         val taskDesc = ser.deserialize[TaskDescription](data.value)
         logInfo("Got assigned task " + taskDesc.taskId)
@@ -100,7 +100,7 @@ private[spark] class CoarseGrainedExecutorBackend(
     case KillTask(taskId, _, interruptThread) =>
       if (executor == null) {
         logError("Received KillTask command but executor was null")
-        System.exit(1)
+        exitExecutor(1)
       } else {
         executor.killTask(taskId, interruptThread)
       }
@@ -120,7 +120,7 @@ private[spark] class CoarseGrainedExecutorBackend(
   override def onDisconnected(remoteAddress: RpcAddress): Unit = {
     if (driver.exists(_.address == remoteAddress)) {
       logError(s"Driver $remoteAddress disassociated! Shutting down.")
-      System.exit(1)
+      exitExecutor(1)
     } else {
       logWarning(s"An unknown ($remoteAddress) driver disconnected.")
     }
@@ -133,6 +133,13 @@ private[spark] class CoarseGrainedExecutorBackend(
       case None => logWarning(s"Drop $msg because has not yet connected to driver")
     }
   }
+
+  /**
+   * This function can be overloaded by other child classes to handle
+   * executor exits differently. For e.g. when an executor goes down,
+   * back-end may not want to take the parent process down.
+   */
+  protected def exitExecutor(code: Int): Unit = System.exit(code)
 }
 
 private[spark] object CoarseGrainedExecutorBackend extends Logging {
